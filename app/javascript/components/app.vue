8a162b011f6dd921766d129a612fefc6f8308e89
Milan Rafaeli <mbrafaeli@uc.cl>
2020-01-16 11:07:46 -0300

    feat(vue):  add vue

---
+++ app/javascript/components/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id='app'>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "First messsage!"
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
