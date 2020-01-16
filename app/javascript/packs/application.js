57c9952291cfbaba335812c872583b3f53177455
Milan Rafaeli <mbrafaeli@uc.cl>
2020-01-16 12:03:01 -0300

    fixup! initial commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from '../components/app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('app'))
   const app = new Vue({
-    el: 'app',
-    template: '<App/>',
-    components: { App }
-  })
-});
+    el: '#app',
+    render: h => h(App)
+  }).$mount()
+})
