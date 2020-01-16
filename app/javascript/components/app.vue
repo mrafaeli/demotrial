4cbe23acc7d6e6752bedc486da86097152f955b1
Milan Rafaeli <mbrafaeli@uc.cl>
2020-01-16 13:34:25 -0300

    feat(hangman-img): add hangman img rendering

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,19 +1,63 @@
 <template>
   <div id='app'>
-    <p>{{ message }}</p>
+    <div class="game-container">
+      <div class="left-container">
+        <div class="hangman">
+          <img
+            class="hangman__icon"
+            :src="'../assets/hangman_' + strikes"
+          />
+        </div>
+      </div>
+      <div class="right-container">
+        <div
+          class="game-word"
+        >
+        </div>
+        <div class="used-letters"></div>
+        <div class="available-letters">
+          <div
+            v-for="letter in availableLetters"
+            class="available-letters__letter"
+            @click="tryLetter(letter)"
+          >
+            {{ letter }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data: function () {
-      return {
-        message: "First messsage!"
-      }
+import randomWord from '../utils/randomWord';
+
+const LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+export default {
+  data() {
+    return {
+      strikes: 0,
+      currentWord: randomWord().split(""),
+      usedLetters: [],
+      availableLetters: LETTERS,
     }
-  }
+  },
+  methods: {
+  },
+}
 </script>
 
 <style lang="scss">
+
+.game-container {
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+
+.available-letters {
+  display: flex;
+}
 
 </style>
